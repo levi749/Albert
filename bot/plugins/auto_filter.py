0b807bcb5fcd6bdf146ce86e905476455a64d937
levi749 <84772173+levi749@users.noreply.github.com>
2021-06-06 09:17:58 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -121,7 +121,7 @@ async def auto_filter(bot, update):
             
             results.append(
                 [
-                    InlineKeyboardButton(button_text, url=file_link)
+                    InlineKeyboardButton(🔘 Get our channel Link🔘, url=https://t.me/CL_FILMm)
                 ]
             )
         
